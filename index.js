b6a5457925e1311a6274ad68820392268586193e
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 23:49:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function theBeatlesPlay(musicians,instruments)
   }
   return array
 }
+
+function johnLennonFacts(array)
+{
+  var i=0
+  while(i<array.length)
+  {
+    array[i]=`${array[i]} !!!`
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(n)
+{
+  var array={}
+
+  do {
+    array.push("I love the Beatles!")
+  } while (n<15);
+
+  return array
+}
