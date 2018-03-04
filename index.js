180173084c5fadd2ec1ad025d7b50aa01de636fa
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-04 23:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments)
 {
-  var array[]={}
+  var array={}
   for(var i=0;i<musicians.length;i++)
   {
     array.push(`${musicians[i] plays instruments[i]}`)
